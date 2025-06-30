9c65bc2f3f0143d70d8d37909af378b8d2a40dcf
Swann SIESLING <Swann7777777@gmail.com>
2025-06-30 12:51:19 +0200

    Add project files.

---
+++ SwaggTransformer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "tokenizer.h"
+
+
+int main() {
+
+
+	tokenize();
+
+
+	return 0;
+}
---
+++ tokenizer.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+
+
+
+void tokenize() {
+
+
+
+	std::fstream vocabulary("vocabulary.txt");
+
+	std::fstream corpus("corpus.txt");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
